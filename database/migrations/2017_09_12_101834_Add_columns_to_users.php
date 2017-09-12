7a4c74c3debcde01c159b2f573e585e15f6573c1
baozouxi <253120625@qq.com>
2017-09-12 18:35:29 +0800

    start project

---
+++ database/migrations/2017_09_12_101834_Add_columns_to_users.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnsToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('weixin')->nullable();
+            $table->tinyInteger('active')->default('1');
+            $table->string('phone');
+            $table->integer('sort')->default(0)->comment('排序');
+            $table->integer('group_id')->comment('用户组id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
